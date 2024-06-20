aca7ea985a6fd6cca5d245b01a8c0ee2813437bd
Thanyani266 <thakwinda66@gmail.com>
2024-06-20 06:49:31 +0200

    fixed issues with the pages

---
+++ main.js
@@ -0,0 +1,21 @@
+const btnQ = document.querySelector(".btn-ques");
+    const btnQ1 = document.querySelector(".btn-ques1");
+    const btnQ2 = document.querySelector(".btn-ques2");
+    const textAns = document.querySelector(".txt-ans");
+    const textAns1 = document.querySelector(".txt-ans1");
+    const textAns2 = document.querySelector(".txt-ans2");
+
+    const hideShowAnswer = (el, txt) => {
+      el.addEventListener("click", () => {
+        if(txt.style.display === "none"){
+          txt.style.display = "block";
+        }else{
+          txt.style.display = "none";
+        }
+      })
+    }
+
+    hideShowAnswer(btnQ, textAns);
+    hideShowAnswer(btnQ1, textAns1);
+    hideShowAnswer(btnQ2, textAns2);
+
